5bcf58f8d0cfca74d8873cca4b52a6fb017e6d25
Paul Han <paulhan221@gmail.com>
2015-09-04 13:39:31 -0400

    instagram gem, pry

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem "figaro"
 
 gem "instagram"
 
+gem 'pry'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -1,5 +1,5 @@
 class InstagramController < ApplicationController
-
+  require 'pry'
   def connect
     redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
   end
@@ -11,6 +11,9 @@ class InstagramController < ApplicationController
   end
 
   def feed
+    client = Instagram.client(:access_token => session[:access_token])
+    binding.pry
     render 'feed.html.erb'
-  end
+end
+
 end
