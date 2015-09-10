a5343d22a1b626031bcb3f76d1d4405697a2213e
Paul Han <paulhan221@gmail.com>
2015-09-10 12:14:50 -0400

    title, background color

--- app/assets/javascripts/lazy-load.js
+++ app/assets/javascripts/lazy-load.js
@@ -1,7 +1,7 @@
 fetching = false;
 
 document.addEventListener('scroll', function (event) {
-  var bottomFivePercent = (document.body.scrollHeight - window.innerHeight) * .95;
+  var bottomFivePercent = (document.body.scrollHeight - window.innerHeight) * .99;
   if (window.scrollY >= bottomFivePercent) {
     if (fetching == false) {
       $('#loading').css('display', 'block');
